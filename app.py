d33a42ae9fc905e56a89c3dab416c22067eb4a44
Maygan <mnlightstone@gmail.com>
2019-03-11 22:02:51 -0700

    readded database url to app.py

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from random import randrange
 from models import *
 
 app = Flask(__name__)
-
 app.debug = False
 socketio = SocketIO(app)
 usersOnlineDisplayNames = []
 usersOnlineAvatars = []
 FLASK_DEBUG = True
-db = SQLAlchemy(app)
 
+app.config[
+    "SQLALCHEMY_DATABASE_URI"] = "postgres://wwqmvcdhxfhbwe:3f42258582efaa918df489908dd543ca82e87c5bb09425172e488afac2edd3e6@ec2-23-23-173-30.compute-1.amazonaws.com:5432/dbuue0mpf8fbcp"
 
 # Link the Flask app with the database (no Flask app is actually being run yet)
 db.init_app(app)
